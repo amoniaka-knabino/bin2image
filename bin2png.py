769e247f730aac96775b49d40139f6677aadd425
amoniaka-knabino <cat1dog@yandex.ru>
2019-05-22 22:35:13 +0600

    lib doesn't work correct

--- bin2png.py
+++ bin2png.py
@@ -2,18 +2,40 @@ from sys import argv
 from PIL import Image, ImageDraw
 from math import sqrt
 from itertools import zip_longest
-
+from images2gif import writeGif
 
 def main():
     input_filename = argv[1]
-    if len(argv) < 3:
-        image_filename = input_filename + '_.png'
-    else:
-        image_filename = argv[2]
     file_bytes = get_bytes(input_filename)
     file_bytes_tuples = list(grouper(file_bytes, 3, bytes(0)))
     image_size = get_image_size(file_bytes_tuples)
-    make_image(image_filename, image_size, file_bytes_tuples)
+
+    if argv[2]=='--png':
+        if len(argv) < 4:
+            out_filename = input_filename + '_.png'
+        else:
+            out_filename = argv[3]
+        pic = make_image(image_size, file_bytes_tuples)
+        pic.save(out_filename, 'PNG')
+    elif argv[2]=='--gif':
+        if len(argv) > 4:
+            if argv[3] == '-c':
+                shots_count = argv[4]
+                shot_size = int(image_size / shots_count)
+                if len(argv) == 6:
+                    out_filename = argv[5]
+                elif len(argv) == 6:
+                    out_filename = input_filename+'.gif'
+        else:
+            if len(argv) == 4:
+                out_filename = argv[3]
+            elif len(argv) == 3:
+                out_filename = input_filename+'.gif'
+            shots_count = int(image_size/100)
+            shot_size = 100
+        images = get_image_list(file_bytes_tuples, shot_size)
+        print(images)
+        writeGif(out_filename, images, duration=0.2)
 
 
 def get_bytes(filename):
@@ -24,13 +46,19 @@ def get_bytes(filename):
 def get_image_size(bytes_list):
     return int(sqrt(len(bytes_list)))
 
+
+def get_image_list(s_bytes_tuples, shot_size):
+    images_bytes_list = list(grouper(s_bytes_tuples, shot_size**2))
+    return [make_image(shot_size, image_bytes) for image_bytes in images_bytes_list]
+
+
 #from doc
 def grouper(iterable, n, fillvalue=None):
     args = [iter(iterable)] * n
     return zip_longest(*args, fillvalue=fillvalue)
 
 
-def make_image(filename, image_size, s_bytes_tuples):
+def make_image(image_size, s_bytes_tuples):
     pic = Image.new('RGB', (image_size, image_size), (255, 255, 255))
     d = ImageDraw.ImageDraw(pic)
     for i in range(0, image_size):
@@ -38,7 +66,8 @@ def make_image(filename, image_size, s_bytes_tuples):
             current_pic = i*image_size + j
             current_color = s_bytes_tuples[current_pic]
             d.point(xy=[(i, j)], fill=current_color)
-    pic.save(filename, "PNG")
+    #pic.save('aa.png', "PNG")
+    return pic
 
 
 if __name__ == "__main__":
