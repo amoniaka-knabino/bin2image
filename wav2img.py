f7415fdde9992e8f77b2373c3d4108664fc8dc17
amoniaka-knabino <cat1dog@yandex.ru>
2019-05-26 23:46:35 +0600

    work with big file and square img

--- wav2img.py
+++ wav2img.py
@@ -6,23 +6,25 @@ from math import sqrt
 
 #https://audiocoding.ru/article/2008/05/22/wav-file-structure.html
 def main():
-    input_filename = argv[1]
+    #input_filename = argv[1]
+    input_filename = 'water_drops.wav'
     samples = get_samples(input_filename, 4096)
     colors = list([(i,i,i) for i in samples])
     image = make_square_image(int(sqrt(len(colors))), colors)
     #image = make_line_image(colors)
-    image.save('test_wav1.png', 'PNG')
+    image.save('test_wav2.png', 'PNG')
     
 
 def get_samples(filename, n=None):
     f = open(filename,'rb')
     byte_per_sample = get_num_bit_per_sample(f) //8
     num_of_channels = get_num_of_channels(f)
-    data = get_data(f, n)
+    data = get_data(f)
+    f.close()
     one_channel_samples = list(grouper(data, byte_per_sample))
     all_channel_samples = list(grouper(one_channel_samples, num_of_channels))
     list_of_list_of_sum = [ [sum(j) for j in i] for i in all_channel_samples ]
-    merged_list = colors =list(chain.from_iterable(list_of_list_of_sum))
+    merged_list = list(chain.from_iterable(list_of_list_of_sum))
     list_of_averages = list([i // (byte_per_sample*num_of_channels) for i in merged_list])
     return(list_of_averages)
     
@@ -40,9 +42,15 @@ def get_num_of_channels(wav_file):
     wav_file.seek(22)
     return(int.from_bytes(wav_file.read(2), byteorder='little'))
 
-def get_data(wav_file, n=None):
+def get_data(wav_file):
+    part_size = 1024
+    file_size = wav_file.seek(0, 2)
     wav_file.seek(44)
-    return(wav_file.read(n))
+    parts_of_file = []
+    for i in range(0, file_size//part_size):
+        info = wav_file.read(part_size)
+        parts_of_file.append(info)
+    return(list(chain.from_iterable(parts_of_file)))
 
 def make_square_image(image_size, s_bytes_tuples):
     pic = Image.new('RGB', (image_size, image_size), (255, 255, 255))
